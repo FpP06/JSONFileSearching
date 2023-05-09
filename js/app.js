cc21f14f5f38943efb6888fd33b90e67f3af7274
FpP06 <pisniak21.filip@gmail.com>
2023-05-09 14:49:27 +0200

    FpP06

---
+++ js/app.js
@@ -0,0 +1,96 @@
+document.addEventListener('DOMContentLoaded', init, false);
+
+let data, sortCol;
+let sortAsc = false;
+const pageSize = 20;
+let curPage = 1;
+let table = document.getElementById('table');
+let tbody = document.querySelector('#tableNames tbody');
+let thead = document.querySelector('#tableNames thead');
+async function init() {
+    
+    let response = await fetch('../json/posortowane_imiona.json');
+    data = await response.json();
+    data = Object.values(data)
+    createTable(data);
+    let input = document.getElementById('name');
+    let btnGr = document.querySelector('.btn-group');
+    input.addEventListener('input', function() {
+        btnGr.style.display = "inline-flex";
+        thead.style.display = "table-header-group";
+        if(this.value.length === 0) {
+            createTable(data);
+        }
+        else if(check(this.value)) {
+            createTableFound(this.value);
+            btnGr.style.display = "none";
+        }
+        else {
+            tbody.innerHTML = "<div class='alert alert-danger' role='alert'>Nie znaleziono żadnych wyników dla wybranych kryteriów.</div>";
+            thead.style.display = "none";
+            btnGr.style.display = "none";
+        }
+    });
+    document.querySelectorAll('#tableNames thead tr th').forEach(t => {
+        t.addEventListener('click', sort, false);
+    });
+    document.querySelector('#next').addEventListener('click', nextPage, false);
+    document.querySelector('#prev').addEventListener('click', previousPage, false);
+}
+
+function createTable() {
+    let result = '';
+    data.filter((row, index) => {
+        let start = (curPage-1)*pageSize;
+        let end =curPage*pageSize;
+        if(index >= start && index < end) return true;
+    }).forEach(x => {
+        result += `<tr>
+        <td>${x.nr}</td>
+        <td>${x.name}</td>
+        <td>${x.s}</td>
+        <td>${x.count.toLocaleString()}</td>
+        </tr>`;
+    });
+    tbody.innerHTML = result;
+}
+
+function createTableFound(arg) {
+    let arr_ob = data.filter(x => x.name.startsWith(arg.toUpperCase()));
+    let result = '';
+    for(let x of arr_ob) {
+        result += `<tr>
+        <td>${x.nr}</td>
+        <td>${x.name}</td>
+        <td>${x.s}</td>
+        <td>${x.count.toLocaleString()}</td>
+        </tr>`;
+    }
+    tbody.innerHTML = result;
+}
+
+function sort(e) {
+    let thisSort = e.target.dataset.sort;
+    if(sortCol === thisSort) sortAsc = !sortAsc;
+    sortCol = thisSort;
+    data.sort((a, b) => {
+      if(a[sortCol] < b[sortCol]) return sortAsc?1:-1
+      if(a[sortCol] > b[sortCol]) return sortAsc?-1:1;
+      return 0;
+    });
+    createTable();
+}
+
+function previousPage() {
+    if(curPage > 1) curPage--;
+    createTable();
+}
+  
+function nextPage() {
+    if((curPage * pageSize) < data.length) curPage++;
+    createTable();
+}
+
+function check(value) {
+    return data.some(x => x.name.startsWith(value.toUpperCase()));
+}
---
+++ js/updateJSONFile.js
@@ -0,0 +1,26 @@
+// node updateJSONFile.js
+
+const fs = require('fs');
+
+let data = fs.readFileSync('../json/wykaz_imion.json', 'utf8');
+let names = Object.values(JSON.parse(data));
+let arr_obj = [];
+let arr_obj2 = [];
+//console.log(Object.values(names));
+
+let i = 1;
+for(let x of names) {
+    let obj = {};
+    obj.nr = i;
+    obj.name = x.name;
+    obj.s = x.s;
+    obj.count = parseInt(x.count);
+    arr_obj.push(obj);
+    i++;
+}
+
+for(let x of arr_obj) {
+    if(x.name.length >= 3) arr_obj2.push(x);
+}
+
+fs.writeFileSync('../json/posortowane_imiona.json', JSON.stringify(Object.assign({}, arr_obj2), null, 2));
